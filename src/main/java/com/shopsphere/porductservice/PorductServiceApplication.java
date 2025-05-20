e8dec1c9e58e311adc1c042447d487c99d2af24c
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-05-20 08:32:11 +0530

    Init

---
+++ src/main/java/com/shopsphere/porductservice/PorductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopsphere.porductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorductServiceApplication.class, args);
+    }
+
+}
