bf3f400b852e3e32a3a873fbbb2f4e3166915b84
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-05-24 07:52:17 +0530

    contact info details

--- src/main/java/com/shopsphere/porductservice/PorductServiceApplication.java
+++ src/main/java/com/shopsphere/porductservice/PorductServiceApplication.java
@@ -1,11 +1,14 @@
 package com.shopsphere.porductservice;
 
+import com.shopsphere.porductservice.dto.ContactDetailsDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {ContactDetailsDTO.class})
 public class PorductServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/shopsphere/porductservice/dto/ContactDetailsDTO.java
@@ -0,0 +1,18 @@
+package com.shopsphere.porductservice.dto;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@ConfigurationProperties(prefix = "contact")
+public class ContactDetailsDTO {
+
+    private String message;
+
+    private Map<String, String> contactDetails;
+
+    private List<String> onCallSupport;
+}
